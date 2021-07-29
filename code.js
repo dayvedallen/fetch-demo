8d787aa6343618e1e72ecb34c0e889f4fa7f8893
mmeacham92 <meacham.michael@rocketmail.com>
2021-07-29 14:40:38 -0500

    set up our eventListener on the button element; set up a static fetch request

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
-console.log('hello');
+// Selecting our HTML elements
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const countryDiv = document.querySelector(".country-div");
+
+// Add an event listener to our button
+button.addEventListener("click", function () {
+  // Fetch our data
+  fetch("https://restcountries.eu/rest/v2/name/usa")
+    .then((response) => response.json())
+    .then((data) => {
+        const countryData = data[0];
+        console.log(countryData);
+        // const [countryData] = data;    // array destructuring
+    });
+});
