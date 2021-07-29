1ef1b5185d44284311bb5ac843957625c7b22905
mmeacham92 <meacham.michael@rocketmail.com>
2021-07-29 15:05:06 -0500

    fetch request is now dynamic and uses input value upon button click; creating and appending an image once fetch request is resolved

--- code.js
+++ code.js
@@ -5,12 +5,19 @@ const countryDiv = document.querySelector(".country-div");
 
 // Add an event listener to our button
 button.addEventListener("click", function () {
+  // Grab what the user types into the input box
+  const country = input.value.trim();
   // Fetch our data
-  fetch("https://restcountries.eu/rest/v2/name/usa")
+  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
     .then((response) => response.json())
     .then((data) => {
-        const countryData = data[0];
-        console.log(countryData);
-        // const [countryData] = data;    // array destructuring
+      const countryData = data[0];
+      console.log(countryData);
+      // const [countryData] = data;    // array destructuring
+
+      const image = document.createElement("img");
+      image.src = countryData.flag;
+      image.classList.add("flag");
+      document.body.append(image);
     });
 });
